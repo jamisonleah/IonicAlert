a9212b0fa9f7cb1ee89268e5eac11c0364a79bf5
Leah Jamison <lxj9480@rit.edu>
2020-07-19 22:28:11 -0400

    Added and Configured Server.js to run on heroku

--- src/pages/mainAlert.tsx
+++ src/pages/mainAlert.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
@@ -8,16 +8,11 @@ const Tab1: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle> Alerts </IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+      <IonButton color="tertiary" expand="block"> Paul Spotted </IonButton>
       </IonContent>
     </IonPage>
   );
